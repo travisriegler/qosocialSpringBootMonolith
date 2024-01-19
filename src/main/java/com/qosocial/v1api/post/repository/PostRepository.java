556e692d248b65ae60caf15e048adde876b36b12
Travis Riegler <travisriegler@protonmail.com>
2024-01-19 17:03:41 -0500

    fixed postRepository query

--- src/main/java/com/qosocial/v1api/post/repository/PostRepository.java
+++ src/main/java/com/qosocial/v1api/post/repository/PostRepository.java
@@ -10,10 +10,10 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<PostModel, Long> {
 
-    @Query("SELECT p FROM PostModel p WHERE (p.profileModel.id = :myProfileId OR (p.isDeleted = false AND p.profileModel.id != :myProfileId)) AND p.createdAt < :timeStamp ORDER BY p.createdAt DESC")
+    @Query("SELECT p FROM PostModel p WHERE (p.profileModel.id = :myProfileId OR (p.deleted = false AND p.profileModel.id != :myProfileId)) AND p.createdAt < :timeStamp ORDER BY p.createdAt DESC")
     List<PostModel> findMyPostsAndOthersNotDeletedBeforeTimestamp(Long myProfileId, Instant timeStamp, Pageable pageable);
 
-    @Query("SELECT p FROM PostModel p WHERE p.profileModel.id = :profileId AND (:isMyProfile = true OR p.isDeleted = false) AND p.createdAt < :timeStamp ORDER BY p.createdAt DESC")
+    @Query("SELECT p FROM PostModel p WHERE p.profileModel.id = :profileId AND (:isMyProfile = true OR p.deleted = false) AND p.createdAt < :timeStamp ORDER BY p.createdAt DESC")
     List<PostModel> findPostsByProfileAndOwnershipBeforeTimestamp(Long profileId, boolean isMyProfile, Instant timeStamp, Pageable pageable);
 
 }
